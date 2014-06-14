77071aaed7333869e9be3bcd4a4c56846709afc0
on-three <on_three@outlook.com>
2014-06-13 23:13:45 -0700

    Fixes #11

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 #include <string>
 #include <gst/gst.h>
 #include "relay.hpp"
+#include <tclap/CmdLine.h>
+#include <iostream>
 
+using std::cout;
+using std::endl;
 
 /******************************************************************************
 function: main()
@@ -15,25 +19,38 @@ int main(int argc, char *argv[]) {
   /* Initialize GStreamer */
   gst_init (&argc, &argv);
 
-  //MyStubServer s(&queue);
+  std::string url = "";
+  try {
+    TCLAP::CmdLine cmd("********relay-stream-with-overlay********"
+    BOLDWHITE "Video stream realy with json RPC text overlay server." RESET
+    "If successful, program should report correct stream online status."
+    "Clients (i.e. vlc) can then connect to the relay via TCP on port 10000.\n"
+    BOLDRED "Port value 10000 " RESET "is currently hard coded."
+    "The URL i'm using for VLC is " BOLDWHITE "\"tcp://127.0.0.1:10000\"" RESET
+    "for a vlc instance on the same host."
+    "Some work needs to be done to make a more scalable solution.",
+     ' ', //delimiter between command line args
+     "0.1a"); //version of program
 
-  //draw url to relay out of our command line args
-  if(argc<2) {
-    g_print ("********relay-stream-with-overlay********"
-    BOLDWHITE "\nusage: relay-stream-with-overlay [URL of http stream to relay]" RESET
-    "\nIf successful, program should report correct stream online status."
-    "\nClients (i.e. vlc) can then connect to the relay via TCP on port 10000"
-    BOLDRED "\nPort value 10000 " RESET "is currently hard coded."
-    "\nThe URL i'm using for VLC is " BOLDWHITE "\"tcp://127.0.0.1:10000\"" RESET
-    "\nfor a vlc instance on the same host."
-    "\nSome work needs to be done to make a more scalable solution.");
+    //primary positional arg is the URL of the video stream.
+    //TCLAP::UnlabeledValueArg< std::string >  stream_url( "url", "URL of video stream to relay.", true, "std::string");
+    TCLAP::UnlabeledValueArg< std::string > stream_url(std::string("url"),
+      std::string("URL of video stream to relay."),
+      true,//required
+      std::string(""),
+      std::string("URL of video stream to realay"));
+    cmd.add(stream_url);
+
+    cmd.parse(argc, argv);
+    url = stream_url.getValue();
+
+  } catch (TCLAP::ArgException &e)  {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << endl;
     return -1;
   }
 
-  const std::string uri = argv[1];
-
   try {
-    Relay relay(std::string("my_relay"), uri);
+    Relay relay(std::string("my_relay"), url);
 
     relay.Initialize();
     relay.Run();
